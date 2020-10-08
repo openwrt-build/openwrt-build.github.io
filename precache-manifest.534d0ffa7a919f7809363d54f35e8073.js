7c2ce55b2f26be2e19e6b0ec4ed0a5314e620c2d
OpenWrtBuild <OpenWrtBuild@github.io>
2020-10-08 22:45:52 +0800

    Updates

---
+++ precache-manifest.534d0ffa7a919f7809363d54f35e8073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ae8303d1718f2cc08d44284cdb2031",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb9e306402d9e85e0276",
+    "url": "./static/css/2.cf4232a4.chunk.css"
+  },
+  {
+    "revision": "5162ab98ff66fd75a9de",
+    "url": "./static/css/main.e56057e7.chunk.css"
+  },
+  {
+    "revision": "eb9e306402d9e85e0276",
+    "url": "./static/js/2.9495b869.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.9495b869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5162ab98ff66fd75a9de",
+    "url": "./static/js/main.ec6757a3.chunk.js"
+  },
+  {
+    "revision": "94fc5d58e4f2b9200c7b",
+    "url": "./static/js/runtime-main.235ff7b3.js"
+  }
+]);
